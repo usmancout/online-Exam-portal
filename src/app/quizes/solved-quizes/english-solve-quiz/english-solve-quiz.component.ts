c1f8c1b240593e73c558188eb2b233d937685b59
Faseeh <fashiali412@gmail.com>
2024-12-04 23:08:19 +0500

    Done settings in html and css of login and sign up page

--- src/app/quizes/solved-quizes/english-solve-quiz/english-solve-quiz.component.ts
+++ src/app/quizes/solved-quizes/english-solve-quiz/english-solve-quiz.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-english-solve-quiz',
   imports: [],
   templateUrl: './english-solve-quiz.component.html',
+  standalone: true,
   styleUrl: './english-solve-quiz.component.css'
 })
 export class EnglishSolveQuizComponent {
